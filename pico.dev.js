9e71586a2013c70263760d8f69ab8b009ec5852c
mohayonao <mohayonao@gmail.com>
2013-03-10 15:23:13 +0900

    fixed: use the window namespace

--- pico.dev.js
+++ pico.dev.js
@@ -1,29 +1,36 @@
 (function() {
     "use strict";
-    
+
     var ImplClass = null;
-    if (typeof webkitAudioContext !== "undefined") {
+
+    var AudioContext = (function() {
+        if (typeof window !== "undefined") {
+            return window.webkitAudioContext || window.AudioContext;
+        }
+    })();
+
+    if (typeof AudioContext !== "undefined") {
         ImplClass = function(sys) {
-            var context = new webkitAudioContext();
+            var context = new AudioContext();
             var bufSrc, jsNode;
 
             this.maxSamplerate     = context.sampleRate;
             this.defaultSamplerate = context.sampleRate;
             this.env = "webkit";
-            
+
             var ua = navigator.userAgent;
             if (ua.match(/linux/i)) {
                 sys.streammsec *= 8;
             } else if (ua.match(/win(dows)?\s*(nt 5\.1|xp)/i)) {
                 sys.streammsec *= 4;
             }
-            
+
             this.play = function() {
                 var onaudioprocess;
                 var jsn_streamsize = sys.getAdjustSamples(context.sampleRate);
                 var sys_streamsize = sys.streamsize;
                 var x, dx;
-                
+
                 if (sys.samplerate === context.sampleRate) {
                     onaudioprocess = function(e) {
                         var outs = e.outputBuffer;
@@ -40,7 +47,7 @@
                         var outR = outs.getChannelData(1);
                         var i, imax = outs.length;
                         var j;
-                        
+
                         sys.process();
                         for (i = j = 0; i < imax; i += 2, ++j) {
                             outL[i] = outL[i+1] = inL[j];
@@ -57,7 +64,7 @@
                         var outL = outs.getChannelData(0);
                         var outR = outs.getChannelData(1);
                         var i, imax = outs.length;
-                        
+
                         for (i = 0; i < imax; ++i) {
                             if (x >= sys_streamsize) {
                                 sys.process();
@@ -69,7 +76,7 @@
                         }
                     };
                 }
-                
+
                 bufSrc = context.createBufferSource();
                 jsNode = context.createJavaScriptNode(jsn_streamsize, 2, sys.channels);
                 jsNode.onaudioprocess = onaudioprocess;
@@ -77,7 +84,7 @@
                 bufSrc.connect(jsNode);
                 jsNode.connect(context.destination);
             };
-            
+
             this.pause = function() {
                 bufSrc.disconnect();
                 jsNode.disconnect();
@@ -89,14 +96,14 @@
             var timer = (function() {
                 var source = "var t=0;onmessage=function(e){if(t)t=clearInterval(t),0;if(typeof e.data=='number'&&e.data>0)t=setInterval(function(){postMessage(0);},e.data);};";
                 var blob = new Blob([source], {type:"text/javascript"});
-                var path = URL.createObjectURL(blob);
+                var path = window.URL.createObjectURL(blob);
                 return new Worker(path);
             })();
 
             this.maxSamplerate     = 48000;
             this.defaultSamplerate = 44100;
             this.env = "moz";
-            
+
             this.play = function() {
                 var audio = new Audio();
                 var interleaved = new Float32Array(sys.streamsize * sys.channels);
@@ -104,7 +111,7 @@
                 var written  = 0;
                 var writtenIncr = sys.streamsize / sys.samplerate * 1000;
                 var start = Date.now();
-                
+
                 var onaudioprocess = function() {
                     if (written > Date.now() - start) {
                         return;
@@ -121,12 +128,12 @@
                     audio.mozWriteAudio(interleaved);
                     written += writtenIncr;
                 };
-                
+
                 audio.mozSetup(sys.channels, sys.samplerate);
                 timer.onmessage = onaudioprocess;
                 timer.postMessage(streammsec);
             };
-            
+
             this.pause = function() {
                 timer.postMessage(0);
             };
@@ -140,14 +147,14 @@
             this.pause = function() {};
         };
     }
-    
+
     var ACCEPT_SAMPLERATES = [
         8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
     ];
     var ACCEPT_CELLSIZES = [
         32,64,128,256
     ];
-    
+
     function SoundSystem(opts) {
         this.impl = null;
         this.isPlaying  = false;
@@ -156,10 +163,10 @@
         this.cellsize   = 128;
         this.streammsec = 20;
         this.streamsize = 0;
-        
+
         this.generator  = null;
     }
-    
+
     SoundSystem.prototype.bind = function(klass, opts) {
         if (typeof klass === "function") {
             var player = new klass(this, opts);
@@ -172,7 +179,7 @@
         }
         return this;
     };
-    
+
     SoundSystem.prototype.setup = function(params) {
         if (typeof params === "object") {
             if (ACCEPT_SAMPLERATES.indexOf(params.samplerate) !== -1) {
@@ -198,12 +205,11 @@
                 this.cellsize = 256;
                 break;
             }
-        } else {
-            
+
         }
         return this;
     };
-    
+
     SoundSystem.prototype.getAdjustSamples = function(samplerate) {
         var samples, bits;
         samplerate = samplerate || this.samplerate;
@@ -212,24 +218,24 @@
         bits = (bits < 8) ? 8 : (bits > 14) ? 14 : bits;
         return 1 << bits;
     };
-    
+
     SoundSystem.prototype.play = function(generator) {
         if (this.isPlaying || typeof generator !== "object") {
             return this;
         }
         this.isPlaying = true;
-        
+
         this.generator  = generator;
         this.streamsize = this.getAdjustSamples();
         this.strmL = new Float32Array(this.streamsize);
         this.strmR = new Float32Array(this.streamsize);
         this.cellL = new Float32Array(this.cellsize);
         this.cellR = new Float32Array(this.cellsize);
-        
+
         this.impl.play();
         return this;
     };
-    
+
     SoundSystem.prototype.pause = function() {
         if (this.isPlaying) {
             this.isPlaying = false;
@@ -237,7 +243,7 @@
         }
         return this;
     };
-    
+
     SoundSystem.prototype.process = function() {
         var cellL = this.cellL, cellR = this.cellR;
         var strmL = this.strmL, strmR = this.strmR;
@@ -245,7 +251,7 @@
         var i, imax = cellL.length;
         var j = 0, n = this.streamsize / this.cellsize;
         var x;
-        
+
         while (n--) {
             generator.process(cellL, cellR);
             for (i = 0; i < imax; ++i, ++j) {
@@ -256,9 +262,9 @@
             }
         }
     };
-    
+
     var instance = new SoundSystem().bind(ImplClass);
-    
+
     var exports = {
         setup: function(opts) {
             instance.setup(opts);
@@ -304,7 +310,7 @@
             }
         }
     });
-    
+
     if (typeof module !== "undefined" && module.exports) {
         module.exports = global.pico = exports;
     } else if (typeof window !== "undefined") {
@@ -339,7 +345,7 @@
                 return a;
             };
         }
-        
+
         exports.noConflict = (function() {
             var _ = window.pico;
             return function() {
