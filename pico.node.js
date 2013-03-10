09e295cb71fd480bf60a04b8ad4959de9823d30b
mohayonao <mohayonao@gmail.com>
2013-03-10 15:33:32 +0900

    moved

--- pico.node.js
+++ pico.node.js
@@ -15,21 +15,21 @@ var Speaker = require("speaker");
 if (!Readable) Readable = require("readable-stream/readable");
 
 function PicoNodePlayer(sys) {
-    
+
     this.maxSamplerate     = 48000;
     this.defaultSamplerate = 44100;
     this.env = "node";
     this.node = null;
-    
+
     this.play = function() {
         this.node = new Readable();
         this.node._read = function(n, fn) {
             var inL = sys.strmL, inR = sys.strmR;
             var buf = new Buffer(n);
-            
+
             var i, j = 0;
             var imax = inL.length;
-            
+
             n = (n >> 2) / sys.streamsize;
             while (n--) {
                 sys.process();
@@ -40,18 +40,15 @@ function PicoNodePlayer(sys) {
                     j += 2;
                 }
             }
-            
+
             fn(null, buf);
         };
         this.node.pipe(new Speaker({sampleRate:sys.samplerate}));
     };
-    
+
     this.pause = function() {
         process.nextTick(this.node.emit.bind(this.node, "end"));
     };
 }
 
-module.exports = PicoNodePlayer;
-if (global.pico) {
-    global.pico.NodePlayer = PicoNodePlayer;
-}
+module.exports = require("./pico.dev").bind(PicoNodePlayer);
