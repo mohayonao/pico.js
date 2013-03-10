7fce6fe6f3cff9146bc8fb15a395e827ad98fd44
mohayonao <mohayonao@gmail.com>
2013-03-10 15:21:32 +0900

    use grunt instead of make

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = function(grunt) {
+    
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    
+    grunt.initConfig({
+        jshint: {
+            all: ["pico.dev.js"],
+            options: {
+                curly   : true,
+                eqeqeq  : true,
+                latedef : true,
+                noarg   : true,
+                noempty : true,
+                quotmark: "double",
+                undef   : true,
+                strict  : true,
+                trailing: true,
+                newcap  : false,
+                browser : true,
+                node    : true,
+                predef  : ["timbre"]
+            }
+        },
+        uglify: {
+            all: {
+                options: { sourceMap: "pico.js.map" },
+                files: { "pico.js": ["pico.dev.js"] }
+            }
+        },
+        watch: {
+            src: {
+                files: ["pico.dev.js"],
+                tasks: ["jshint"]
+            }
+        },
+        clean: ["pico.js", "pico.js.map"]
+    });
+    
+    grunt.registerTask("default", ["jshint", "uglify"]);
+};
--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	uglifyjs -m -o pico.js pico.dev.js
-
-jshint:
-	jshint pico.dev.js libs/*.js
